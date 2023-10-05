24d431ab19fd55a52bd95e1f80a20a63c03c511e
VladislavShakhov <91606973+VladislavSHAKHOV@users.noreply.github.com>
2023-10-05 18:55:45 +0300

    animation fixed

--- scripts/index.js
+++ scripts/index.js
@@ -289,27 +289,37 @@ statBlock.addEventListener("mouseenter", statAnimation);
 // Anpnymus animation
 const anonymusBlock = document.querySelector(".anonymus__block-wrapper");
 const anonymusContent = document.querySelectorAll(".anonymus__content");
+let isAnimatingAnonymus = false; 
 
 function anonymusAnimation() {
-  anonymusContent[0].classList.add("hidden")
-  anonymusContent[1].classList.remove("closed")
-  setTimeout(() => {
-    anonymusContent[0].classList.add("closed")
-    anonymusContent[0].classList.remove("hidden")
-    anonymusContent[1].classList.add("hidden")
-    anonymusContent[2].classList.remove("closed")
+  if (isAnimatingAnonymus) {
+    return;
+  }
 
+  isAnimatingAnonymus = true; 
+
+  anonymusContent[0].classList.add("hidden");
+  anonymusContent[1].classList.remove("closed");
+
+  setTimeout(() => {
+    anonymusContent[0].classList.add("closed");
+    anonymusContent[0].classList.remove("hidden");
+    anonymusContent[1].classList.add("hidden");
+    anonymusContent[2].classList.remove("closed");
   }, 1000);
+
   setTimeout(() => {
-    anonymusContent[2].classList.add("hidden")
-    anonymusContent[0].classList.remove("closed")
-    anonymusContent[1].classList.add("closed")
-    anonymusContent[1].classList.remove("hidden")
+    anonymusContent[2].classList.add("hidden");
+    anonymusContent[0].classList.remove("closed");
+    anonymusContent[1].classList.add("closed");
+    anonymusContent[1].classList.remove("hidden");
   }, 2000);
+
   setTimeout(() => {
-    anonymusContent[2].classList.remove("hidden")
-    anonymusContent[2].classList.add("closed")
+    anonymusContent[2].classList.remove("hidden");
+    anonymusContent[2].classList.add("closed");
+    isAnimatingAnonymus = false; 
   }, 2400);
 }
+
 anonymusBlock.addEventListener("mouseenter", anonymusAnimation);
-  
